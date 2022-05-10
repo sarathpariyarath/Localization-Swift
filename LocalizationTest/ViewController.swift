59b26417346d1b2daddbe0b4888bcbf7705a529f
sarathpariyarath <sarath.pariyarath@gmail.com>
2022-05-10 16:21:20 +0530

    Spanish localization

--- LocalizationTest/ViewController.swift
+++ LocalizationTest/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
-
+let buttonText = NSLocalizedString("button-title", comment: "")
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sampleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        sampleLabel.text = buttonText
     }
 
 
+    @IBAction func engSelected(_ sender: Any) {
+        sampleLabel.text = "button-title".localizableString("en")
+    }
+    @IBAction func spaSelected(_ sender: Any) {
+        sampleLabel.text = "button-title".localizableString("es")
+    }
+}
+
+extension String {
+    func localizableString(_ name: String) -> String {
+        let path = Bundle.main.path(forResource: name, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: "", comment: "")
+    }
 }
 
